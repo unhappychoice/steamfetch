6e9a7a78b4939e9318676a5cfaf675f63df3742c
Yuji Ueki <unhappychoice@gmail.com>
2026-02-13 21:11:29 +0900

    fix: add rpath for Homebrew lib directory to resolve libsteam_api (#24)

--- build.rs
+++ build.rs
@@ -1,8 +1,15 @@
 fn main() {
     // Set rpath so the binary looks for Steam API library in the same directory
+    // and in ../lib/steamfetch/ (for Homebrew: bin/ -> lib/steamfetch/)
     #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+    {
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../lib/steamfetch");
+    }
 
     #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path");
+    {
+        println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib/steamfetch");
+    }
 }
